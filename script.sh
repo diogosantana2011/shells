239b4012725101f5952c3e12c8ffa067e870d807
diogosantana2011 <diegsan20@gmail.com>
2023-12-08 23:22:03 +0100

    (TASK): Array, loops and interpolation - sh

--- script.sh
+++ script.sh
@@ -1,5 +1,41 @@
-cd ~ 
+#!/bin/bash
+
+cd ~ || exit
 pwd
-ls -la
+# ls -la
 greeting='Helloooo from shell script!'
-ECHO $greeting
+echo "$greeting"
+echo 'Another console print via bash.'
+
+# WHO ARE YOU
+echo 'who are you? '
+# who="Diogo Santana"
+read -r who
+echo "Hello, $who!"
+
+# AGE
+
+echo 'How old are you?'
+
+read -r age
+
+if [ "$age" -gt 20 ]; then
+  echo 'You are overage.'
+else
+  echo 'You are underage.'
+fi
+
+# Loop
+files="/Users/diogosantana/Development/shells/*"
+
+for file in $files; do
+  echo $(basename "$file")
+done
+
+# Array
+declare -a array=('This' 'is' 'an' 'array')
+
+# Looping through array
+for item in "${array[@]}"; do
+  echo "$item"
+done
