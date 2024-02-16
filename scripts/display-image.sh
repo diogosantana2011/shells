e1803f674b188e4f555b274dfcdf9e76ac94064a
diogosantana2011 <diegsan20@gmail.com>
2024-02-16 09:48:12 +0000

    (TASK): Adding display img (failing)

---
+++ scripts/display-image.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd ~ && cd ..
+cd $USER
+source .iterm2_shell_integration.bash
+cd ~/Development/shells/scripts/
+imgcat img/pepe.jpg # FAILURE HERE AS TERMINAL NOT RECOGNIZING IMGCAT
